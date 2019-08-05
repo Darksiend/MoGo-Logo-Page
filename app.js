bce6e62f7dbd53400c83a3be790e03d15960b556
Anton <logosmk2@gmail.com>
2019-08-05 23:30:24 +0300

    JS
    accardeon #3

--- app.js
+++ app.js
@@ -47,12 +47,14 @@ $(function () {
 
 
     /*Collapse*/
-    $("[data-collapse").on("click",function(event){
+    $("[data-collapse").on("click", function (event) {
         event.preventDefault();
         var $this = $(this);
         var blockId = $this.data('collapse');
 
 
+
+        $this.toggleClass("active");
         $(blockId).slideToggle();
     });
 });
