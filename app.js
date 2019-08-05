2dd877a68ab139a3d52f820655fdb9299311ea30
Anton <logosmk2@gmail.com>
2019-08-05 17:32:48 +0300

    Add JS
    Add Js for Header

---
+++ app.js
@@ -0,0 +1,39 @@
+$(function () {
+    var header = $("#header");
+    var introH = $("#intro").innerHeight();
+    var scrollOffset = $(window);
+    checkScroll(scrollOffset);
+    $(window).on("scroll", function () {
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        scrollOffset = $(this).scrollTop();
+        if (scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+        var $this = $(this);
+        var blockId = $this.data('scroll');
+        var blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500)
+    })
+
+
+});
