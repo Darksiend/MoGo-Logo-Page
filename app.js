8c597264e57ad34b7524c7823cefe84459f5b51f
Anton <logosmk2@gmail.com>
2019-08-05 17:58:25 +0300

    JQuery
    Add JS to Burger Menu

--- app.js
+++ app.js
@@ -36,4 +36,12 @@ $(function () {
     })
 
 
+    /*Menu nav toggle*/
+
+    $("#nav_toggle").on('click', function (event) {
+        event.preventDefault();
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    })
+
 });
